7ae5c5778954d79c91d30b3e1ee738d17da4cb71
Jeff Crockett <jeffmcrockett@gmail.com>
2021-06-08 18:39:01 -0500

    setting up onclickhandler

--- src/App.js
+++ src/App.js
@@ -1,25 +1,43 @@
-import logo from './logo.svg';
+import React from 'react';
+import { Component } from 'react';
+import axios from 'axios';
+
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  constructor() {
+    super()
+    this.state = {
+      isClicked: false,
+      todos: [],
+      text: ''
+    }
+  }
+
+  handleClick(app) {
+    const id = app.isClicked;
+    this.setState({
+      if (this.state.isClicked = false) {
+        this.setState : true;
+      } else {
+        this.setState = false;
+      }
+    })
+  }
+
+  render() {
+    return (
+      <div className="App">
+        <header className="Header">
+          <form>
+            <button className="handlerButton" value="submit" onClick={this.handleClick}>CLICK ME!</button>
+          </form> 
+          <ol>
+          </ol>
+        </header>
+      </div>
+    )
+  };
 }
 
 export default App;
